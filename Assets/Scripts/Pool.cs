d1f17579521ba37f865da17f1709cd3dd6333ba3
pavelsergaziev <34075975+pavelsergaziev@users.noreply.github.com>
2020-02-01 15:26:14 +0300

    Сделал спавнер менеджер.

---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> where T: MonoBehaviour
+{
+    private Queue<T> _pool;
+    private GameObject _prefab;
+    private Transform _parent;
+    private bool _extendable;
+
+    public Pool(GameObject prefab, int initialPoolSize, Transform objectsParent, bool extendable = false)
+    {
+        _prefab = prefab;
+        _parent = objectsParent;
+        _extendable = extendable;
+
+        _pool = new Queue<T>(initialPoolSize);
+
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject obj = InstantiatePrefab();
+            obj.GetComponent<MeshRenderer>().enabled = false;
+            _pool.Enqueue(obj.GetComponent<T>());
+        }
+    }
+
+    private GameObject InstantiatePrefab()
+    {
+        return Object.Instantiate(_prefab, _parent);
+    }
+
+    public void ReturnObjectToPool(T obj)
+    {
+        _pool.Enqueue(obj);
+    }
+
+    public T GetObjectFromPool()
+    {
+        T obj = null;
+
+        if (_pool.Count > 0)
+        {
+            obj = _pool.Dequeue();
+        }
+        else if(_extendable)
+        {
+            obj = InstantiatePrefab().GetComponent<T>();
+        }
+
+        return obj;
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Arty;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField] private Spawner[] _spawners;
+    [SerializeField] private int _enemiesMaxCount;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _maxRandomDistanceFromSpawnerOnSpawn;
+
+    private Pool<Enemy> _enemyPool;
+
+    public event Action<Enemy> OnEnemySpawn = delegate { };
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _enemyPool = new Pool<Enemy>(_prefab, _enemiesMaxCount, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        for (int i = 0; i < _spawners.Length; i++)
+        {
+            _spawners[i].TimeToActive -= deltaTime;
+
+            if (_spawners[i].TimeToActive <= 0 )
+            {
+                Spawn(_spawners[i].transform.position);
+                _spawners[i].TimeToActive = _spawners[i].TimeToSpawn;
+            }
+        }
+    }
+
+    private void Spawn(Vector3 spawnPosition)
+    {
+        Enemy enemy = _enemyPool.GetObjectFromPool();
+        if (enemy == null)
+            return;
+
+        enemy.transform.position = new Vector3(RandomizeCoordinate(spawnPosition.x), spawnPosition.y, RandomizeCoordinate(spawnPosition.z));
+        enemy.GetComponent<MeshRenderer>().enabled = true;
+
+        OnEnemySpawn.Invoke(enemy);
+    }
+
+    private float RandomizeCoordinate(float coordinate)
+    {
+        return UnityEngine.Random.Range(coordinate - _maxRandomDistanceFromSpawnerOnSpawn, coordinate + _maxRandomDistanceFromSpawnerOnSpawn);
+    }
+
+}
