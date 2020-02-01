f43682e33683a89be1308cefa5877a7dfff516a7
Artty <levshaelody@gmail.com>
2020-02-01 12:54:27 +0300

    Spawner carcas

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arty
+{
+    internal class SpawnManager : MonoBehaviour
+    {
+       
+        private Transform _enemyPool;
+
+        private GameObject[] _enemySpawnersGobj;
+        private Spawner[] _enemySpawners;
+
+        private float _deltaTime;
+
+        private void Awake()
+        {
+            _enemyPool = GameObject.FindGameObjectWithTag("SpawnPool").transform;
+
+            _enemySpawners = FindObjectsOfType<Spawner>();
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Arty
+{
+    public class Spawner : MonoBehaviour
+    {
+        internal GameObject spawnObj;
+        internal Transform spawnTransform;
+
+        internal bool isActive = false;
+
+        /// <summary>
+        /// частота спауна
+        /// </summary>
+        [SerializeField]internal float period;
+
+        [SerializeField] internal float timeToActive;
+
+    }
+}
