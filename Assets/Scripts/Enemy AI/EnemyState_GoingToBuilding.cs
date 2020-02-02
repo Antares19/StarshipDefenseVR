45a7b250775ed4836e58d1c5b377b239c7aef037
pavel <dargoth@narod.ru>
2020-02-02 14:19:54 +0300

    commit

--- Assets/Scripts/Enemy AI/EnemyState_GoingToBuilding.cs
+++ Assets/Scripts/Enemy AI/EnemyState_GoingToBuilding.cs
@@ -8,10 +8,10 @@ public class EnemyState_GoingToBuilding : EnemyState
     public override void Tick(EnemyData enemy, EnemyAI enemyAI)
     {
         //ДВИГАЕМ ВРАГА ПО НАПРАВЛЕНИЮ К ЕГО CurrentTarget
-        enemyAI.Mover.Move(enemy.Transform, enemy.CurrentTarget.transform, enemyAI.EnemySpeedFree * enemyAI.DeltaTime);
+        enemyAI.Mover.Move(enemy.Transform, enemy.TargetBuilding.transform, enemyAI.EnemySpeedFree * enemyAI.DeltaTime);
 
         //Если дошел до дома, то меняем стейт на enemyAI.State_WaitingAtBuilding
-        if (Vector3.Distance(enemy.Transform.position, enemy.CurrentTarget.transform.position) <= enemyAI.DistanceToTargetToCountAsReached)
+        if (Vector3.Distance(enemy.Transform.position, enemy.TargetBuilding.transform.position) <= enemyAI.DistanceToTargetToCountAsReached)
         {
             enemyAI.State_WaitingAtBuilding.OnStateEnter(enemy, enemyAI);
         }
@@ -19,6 +19,7 @@ public class EnemyState_GoingToBuilding : EnemyState
 
     public override void OnStateEnter(EnemyData enemy, EnemyAI enemyAI)
     {
+        Debug.Log(enemy + " идет к дому " + enemy.TargetBuilding);
         enemyAI.TurnEnemyToFaceTarget(enemy);
     }
 
