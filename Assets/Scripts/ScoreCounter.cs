bf3b576d4eb002a57ff3ca2f1236dfeba4cfdfa3
Artty <levshaelody@gmail.com>
2020-02-02 13:13:07 +0300

    ScoreCounter

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ar
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [SerializeField] private Transform _housePivot;
+
+        private int totalHouses;
+        private int currentHouses;
+
+        private void Awake()
+        {
+            totalHouses = _housePivot.childCount;
+            Debug.Log(string.Format("Количество домов на местах: {0}", totalHouses));
+
+            currentHouses = totalHouses;
+        }
+    }
+}
