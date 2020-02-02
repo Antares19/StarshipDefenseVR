0b55f1dc90ece3e2d755b8dc689b3ddaf65cb0ce
Artty <levshaelody@gmail.com>
2020-02-02 13:17:08 +0300

    Counter + UI integration

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -11,12 +11,17 @@ namespace Ar
         private int totalHouses;
         private int currentHouses;
 
+        private UI _ui;
+
         private void Awake()
         {
             totalHouses = _housePivot.childCount;
             Debug.Log(string.Format("Количество домов на местах: {0}", totalHouses));
 
             currentHouses = totalHouses;
+
+            _ui = GetComponent<UI>();
+            _ui.SetState(string.Format("{0} / {1}", currentHouses, totalHouses), 120f);
         }
     }
 }
