6fd267582e47882a4d22d9e47d18003cc72ef3bb
Igor Konyakhin <nns2009@yandex.ru>
2020-02-02 13:10:49 +0300

    Basic UI which displays Score and "Time Left"

---
+++ Assets/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UI : MonoBehaviour
+{
+    public TextMesh Score;
+    public TextMesh TimeLeft;
+
+    public void SetState(string scoreString, float secondsLeft)
+    {
+        Score.text = scoreString;
+
+        int iSecondsLeft = (int)secondsLeft;
+        TimeLeft.text = (iSecondsLeft / 60) + ":" + (iSecondsLeft % 60).ToString("00");
+    }
+
+    void Start()
+    {
+        StartCoroutine(Test());
+    }
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(2);
+        SetState("Works", 127.4f);
+    }
+}
